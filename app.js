229072e3ad1e68c501bd3d53754d1631de325009
Trilip <gusilveiratrilip@gmail.com>
2025-08-18 18:03:35 -0300

    feat: adicionada a funcionalidade do sorteio

--- app.js
+++ app.js
@@ -13,45 +13,45 @@ function teclaEnter(event) {
 // Adiciona um EventListener ao input com o ID 'amigo' dentro do HTML
 botao.addEventListener('keyup', teclaEnter);
 
+// Função que adiciona os amigos
 function adicionarAmigo() {
-    mostrarTexto('h2', 'Digite o nome dos seus amigos');
+    mostrarTexto('h2', 'Digite o nome do seu amigos');
 
     // Pega o valor inserido no campo de input do HTML e capitaliza a primeira letra
-    let amigos = document.querySelector('input').value.trim();
-    amigos = amigos.charAt(0).toUpperCase() + amigos.slice(1);
+    let amigo = document.querySelector('input').value.trim();
+    amigo = amigo.charAt(0).toUpperCase() + amigo.slice(1);
 
     // Verifica se o valor inserido no campo de texto é válido e de fato é uma string
-    if (amigos !== "" && amigos !== undefined && isNaN(amigos)) {
+    if (amigo !== "" && amigo !== undefined && isNaN(amigo)) {
         // Verifica se o valor já está dentro da array e retorna a condicional caso esteja
-        if (listaAmigos.includes(amigos)) {
+        if (listaAmigos.includes(amigo)) {
             limparTexto();
             mostrarTexto('h2', 'Esse nome já está na lista!');
             return; 
         }
         // Adiciona o valor inserido para dentro da array separado por quebras de linha
-        listaAmigos.push(amigos);
-        let listaAmigos2 = listaAmigos.join('<br>')
+        listaAmigos.push(amigo);
+        let listaFormatada = listaAmigos.join('<br>')
 
         //Atribui o tamanho da array à variável de limite pra que seja utilizado como um teto na geração de números
         limiteArray = listaAmigos.length
 
         //Adiciona a array atualizada no html
-        adicionarAmigosDaLista('listaAmigos', listaAmigos2);
+        adicionarTextoPorID('listaAmigos', listaFormatada);
         limparTexto();
     } else {
         mostrarTexto('h2', 'Tem que ser um nome de verdade!');
     }  
 }
 
-
 // Função para limpar a barra de inserir nomes.
 function limparTexto() {
-    amigos = document.querySelector('input');
-    amigos.value = '';
+    amigo = document.querySelector('input');
+    amigo.value = '';
 }
 
 //Função específica para adição da lista de nomes ao HTML.
-function adicionarAmigosDaLista(elementID, texto) {
+function adicionarTextoPorID(elementID, texto) {
     let campoLista = document.getElementById(elementID);
     campoLista.innerHTML = texto;
 }
@@ -64,9 +64,12 @@ function mostrarTexto(tag, texto) {
 
 // função que sorteia os índices do array com base em uma geração de números aleatórios limitados ao tamanho da array
 function sortearAmigo() {
-    gerarNumero();
-
+    let numeroIndice = gerarNumero();
+    let escolhido = listaAmigos[numeroIndice];
+    let mensagemFinal = `O amigo secreto sorteado é: ${escolhido}`;
+    adicionarTextoPorID('resultado', mensagemFinal);
 }
+
 // Função pra gerar um número aleatório de acordo com o tamanho da array pra sortear os elementos dentro dela.
 function gerarNumero() {
     let numeroAleatorio = Math.floor(Math.random() * limiteArray);
