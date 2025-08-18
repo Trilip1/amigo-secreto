7aba255c43018396389c87123a51d280b5c152c0
Trilip <gusilveiratrilip@gmail.com>
2025-08-18 18:35:58 -0300

    fix: arrumada a verificação da validade do nome

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let listaAmigos = [];
 let limiteArray;
 
-let botao = document.getElementById('amigo')
+const botao = document.getElementById('amigo')
 
 // Função que recebe um evento como parâmetro, checa se esse evento advém do uso da tecla Enter e executa a função adicionarAmigo();
 function teclaEnter(event) {
@@ -13,7 +13,7 @@ function teclaEnter(event) {
 // Adiciona um EventListener ao input com o ID 'amigo' dentro do HTML
 botao.addEventListener('keyup', teclaEnter);
 
-// Função que adiciona os amigos
+// Função que adiciona os amigos, verifica se a string
 function adicionarAmigo() {
     mostrarTexto('h2', 'Digite o nome do seu amigos');
 
@@ -22,7 +22,7 @@ function adicionarAmigo() {
     amigo = amigo.charAt(0).toUpperCase() + amigo.slice(1);
 
     // Verifica se o valor inserido no campo de texto é válido e de fato é uma string
-    if (amigo !== "" && amigo !== undefined && isNaN(amigo)) {
+    if (amigo !== "" && amigo !== undefined && temLetras(amigo) == true) {
         // Verifica se o valor já está dentro da array e retorna a condicional caso esteja
         if (listaAmigos.includes(amigo)) {
             limparTexto();
@@ -52,21 +52,21 @@ function limparTexto() {
 
 //Função específica para adição da lista de nomes ao HTML.
 function adicionarTextoPorID(elementID, texto) {
-    let campoLista = document.getElementById(elementID);
+    const campoLista = document.getElementById(elementID);
     campoLista.innerHTML = texto;
 }
 
 //Função genérica para inserir texto no HTML a partir de um seletor genérico.
 function mostrarTexto(tag, texto) {
-    let campo = document.querySelector(tag);
+    const campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 // função que sorteia os índices do array com base em uma geração de números aleatórios limitados ao tamanho da array
 function sortearAmigo() {
-    let numeroIndice = gerarNumero();
-    let escolhido = listaAmigos[numeroIndice];
-    let mensagemFinal = `O amigo secreto sorteado é: ${escolhido}`;
+    const numeroIndice = gerarNumero();
+    const escolhido = listaAmigos[numeroIndice];
+    const mensagemFinal = `O amigo secreto sorteado é: ${escolhido}`;
     adicionarTextoPorID('resultado', mensagemFinal);
 }
 
@@ -76,4 +76,7 @@ function gerarNumero() {
     return numeroAleatorio; 
 }
 
-
+function temLetras(parameter) {
+    const regex = /^[a-zA-Z]+$/
+    return regex.test(parameter);
+}
